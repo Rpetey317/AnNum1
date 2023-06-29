c8e968d294b8231e9905caedddbebe53d639032d
rpetey317 <rubenandrelo31@gmail.com>
2023-06-29 17:17:23 -0300

    Añadida la parte del gráfico

--- TP2/inicial.py
+++ TP2/inicial.py
@@ -1,4 +1,7 @@
 import os
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.ticker import MultipleLocator
 
 A, B, C, D = 1.2, 0.6, 0.8, 0.3
 STEP = 0.1
@@ -45,6 +48,25 @@ def pasoRK4(x, y):
     yp1 = y + (STEP/6)*(k1(x, y) + 2*k2(x, y) + 2*k3(x, y) + k4(x, y))
     return xp1, yp1
 
+def generar_grafico(tt, xx, yy, output='output.png'):
+    fig, ax = plt.subplots(figsize=(8, 5), layout='constrained')
+    
+    ax.plot(tt, xx, label='presas')
+    ax.plot(tt, yy, label='depredadores')
+    ax.set_xlim([0, 31])
+    ax.set_ylim([0, 6])
+
+    ax.set_xlabel('tiempo (t)')
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+
+    ax.set_ylabel('población')
+    ax.yaxis.set_minor_locator(MultipleLocator(0.5))
+
+    ax.set_title('Simulación por Runge-Kutta')
+    ax.legend()
+    ax.grid(True)
+    fig.savefig(output, dpi=200)
+
 #=========MAIN==========
 def main():
     t0, x0, y0 = 0, 2, 1
@@ -63,6 +85,9 @@ def main():
         file.write("t;x;y\n")
         for point in zip(tt, xx, yy):
             file.write(f'{point[0]:.5f};{point[1]:.5f};{point[2]:.5f}\n')
+
+    generar_grafico(tt, xx, yy)
+
 
 if __name__ == "__main__":
     main()
